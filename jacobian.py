8134725f3c1e08862228dd30958e831baf524537
John Boersma <johnb@metricmodel.com>
2016-02-12 20:20:39 -0500

    explicating syntax

--- jacobian.py
+++ jacobian.py
@@ -1,5 +1,5 @@
 ''' This is a manual computation of the jacobian of a function
-   demonstrating the use of the scan operation. Usually we 
+   demonstrating the use of the scan method. Usually we 
    would just use the theano.gradient.jacobian method.
 '''
 
@@ -7,6 +7,16 @@ import theano
 import theano.tensor as T
 x = T.dvector()
 y = x ** 2
-J, updates = theano.scan(lambda i, y, x : T.grad(y[i], x), sequences=T.arange(y.shape[0]), non_sequences=[y,x])
-f = theano.function([x], J, updates=updates)
-print f([4,4])
+
+# if, instead of a fixed number of iterations, we want to iterate over an object,
+# that object is specified as a 'sequence'
+# constants in a scan are 'non_sequences'
+# .shape returns and Ivector with the shape of the tensor
+# so y.shape[0] is the  size of y along the first axis
+# a single element has shape 1, not 0
+# arange starts at 0 index and ends at the integer before stop
+# so [1,2,3] has shape 3 and  arange (0,1,2)
+
+J = theano.scan(lambda i, y, x : T.grad(y[i], x), sequences=T.arange(y.shape[0]), non_sequences=[y,x])[0]
+f = theano.function([x], J)
+print f([2,2])
