554c6edd6facb70617628326104566366056f2fd
John Boersma <johnb@metricmodel.com>
2016-02-17 20:53:40 -0500

    syntax clarifications

--- IfElseAndSwitch.py
+++ IfElseAndSwitch.py
@@ -24,11 +24,20 @@ print ftest(mat)
 a,b = T.scalars('a','b')
 x,y = T.matrices('x','y')
 
+# switch takes a tensor as a condition and two input variables
+# switch is elementwise so more general
+# ifelse takes a boolean condition and two variables
+# ifelse evaluates only one output (with cvm linker) so can be faster
+# documentation says cvm is not yet the default but apparently it is now
+
 z_switch = T.switch(T.lt(a,b), T.mean(x), T.mean(y))
 z_lazy = ifelse(T.lt(a,b), T.mean(x), T.mean(y))
 
-f_switch = theano.function([a,b,x,y], z_switch, mode=theano.Mode(linker='vm'))
-f_lazyifelse = theano.function([a,b,x,y], z_lazy, mode=theano.Mode(linker='vm'))
+# vm linker is in t tutorial but does not seem to be described in documentation
+# but cvm is documented and seems a bit faster - using that.
+
+f_switch = theano.function([a,b,x,y], z_switch, mode=theano.Mode(linker='cvm'))
+f_lazyifelse = theano.function([a,b,x,y], z_lazy)
 
 val1 = 0.
 val2 = 1.
@@ -43,9 +52,9 @@ tic = time.clock()
 for i in range(n_times):
     f_switch(val1,val2,big_mat1,big_mat2)
 
-print ('time spent evaluating both values %f sec' % (time.clock() - tic)) 
+print 'time spent evaluating both values %f sec' % (time.clock() - tic)
 
 tic = time.clock()
 for i in range(n_times):
     f_lazyifelse(val1,val2,big_mat1,big_mat2)
-print ('time spend evaluating one value %f sec'  %  (time.clock() - tic))
+print 'time spend evaluating one value %f sec'  %  (time.clock() - tic)
